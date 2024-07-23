9d2d494eeade3cbe08c67ca12565e3b4c1763660
emillycaarolina <emillyjchagas@gmail.com>
2024-07-23 12:23:44 -0300

    Atividade

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Material disponibilizado pelo professor Anderson Macedo -->
+    <meta charset="utf-8">
+    <title>Produtos</title>
+    <link rel="stylesheet" href="css/style.css" media="screen" title="no title" charset="utf-8">
+    <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+  </head>
+  <body>
+    <header>
+      <div class="center">
+        <h1 style="text-align: center">Programação Web II - Pedido de compra</h1>
+        <a href="carrinho.php" target="_blank" style="margin-left: 10px;">Carrinho</a>
+        <a href="categoria.php" target="_self" style="margin-left: 1px;" >Categoria</a>
+        <a href="marca.php" target="_self" style="margin-left: 1px;" >marcas</a>
+        <a href="produtos.php" target="_self" style="margin-left: 1px;" >produtos</a>
+      </div>
+    </header>
+    <section id="produtos">
+        <div class="center">
+          <?php require_once('controller/produtos-busca.php'); ?>
+        </div>
+    </section>
+
+  </body>
+</html>
---
+++ insere-marca.php
@@ -0,0 +1,19 @@
+<?php
+include('controller/conexao.php');
+
+$descricao = $_POST['descricao'];
+
+echo "<h3>Descrição: $descricao</h3><br><br>";
+
+$cad_categoria = "INSERT INTO marca(DESCRICAO) VALUES ('$descricao')";
+
+//query executa comando SQL dentro do banco
+if(mysqli_query($mysqli, $cad_categoria)){
+    echo "<h1>Categoria cadastrada com sucesso!!</h1><br>";
+}else{
+    echo "Erro: " . $cad_categoria . "<br>" . mysqli_error($mysqli);
+}
+
+mysqli_close($mysqli);
+
+?>
